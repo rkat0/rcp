18352c783e89409107800c11da87bb1fa6ed4e5a
rkato <33304527+rkat0@users.noreply.github.com>
2019-01-28 23:59:19 +0900

    introduce tokenization

--- main.rs
+++ main.rs
@@ -8,33 +8,80 @@ fn read_string() -> Vec<char> {
     ret
 }
 
+#[derive(Debug)]
+enum Token {
+    Add,
+    Sub,
+    Num(usize),
+    Eof
+}
+
+impl Token {
+    fn num(&self) -> Option<usize> {
+        match self {
+            Token::Num(n) => Some(*n),
+            _ => None
+        }
+    }
+}
+
 use std::fs::File;
 use std::io::Write;
+use std::process::exit;
 
 fn main() {
     let input = read_string();
-    let mut idx: usize = 0;
     let ofile_name = "tmp.s";
+    let toks = tokenize(&input);
     let mut file = File::create(ofile_name).unwrap();
     file.write_all(b".intel_syntax noprefix\n").unwrap();
     file.write_all(b".global _main\n\n").unwrap();
     file.write_all(b"_main:\n").unwrap();
-    file.write_all(format!("\tmov rax, {}\n", read_num(&input, &mut idx)).as_bytes()).unwrap();
-    while input[idx] != '\0' {
-        if input[idx] == '+' {
-            idx += 1;
-            let n = read_num(&input, &mut idx);
-            file.write_all(format!("\tadd rax, {}\n", n).as_bytes()).unwrap();
-        } else if input[idx] == '-' {
-            idx += 1;
-            let n = read_num(&input, &mut idx);
-            file.write_all(format!("\tsub rax, {}\n", n).as_bytes()).unwrap();
-        } else {
-            println!("expected op (+ or -) but got '{}' (at position {})", input[idx].escape_debug(), idx + 1);
-            return;
+    file.write_all(format!("\tmov rax, {}\n", toks[0].num().unwrap()).as_bytes()).unwrap();
+    let mut i = 1;
+    while i < toks.len() {
+        match toks[i] {
+            Token::Add => {
+                i += 1;
+                file.write_all(format!("\tadd rax, {}\n", toks[i].num().unwrap()).as_bytes()).unwrap();
+                i += 1;
+            },
+            Token::Sub => {
+                i += 1;
+                file.write_all(format!("\tsub rax, {}\n", toks[i].num().unwrap()).as_bytes()).unwrap();
+                i += 1;
+            },
+            Token::Eof => break,
+            _ => {
+                println!("expected op (+ or -) but got token '{:?}'", toks[i]);
+                exit(1);
+            }
         }
     }
     file.write_all(b"\tret\n").unwrap();
+}
+
+fn tokenize(buf: &Vec<char>) -> Vec<Token> {
+    let mut idx = 0;
+    let mut toks = Vec::new();
+    skip_whitespace(buf, &mut idx);
+    while buf[idx] != '\0' {
+        match buf[idx] {
+            '+' => { toks.push(Token::Add); idx += 1; },
+            '-' => { toks.push(Token::Sub); idx += 1; },
+            '0'..='9' => toks.push(Token::Num(read_num(buf, &mut idx))),
+            _ => { println!("unknown character '{}'", buf[idx].escape_debug()); exit(1); }
+        }
+        skip_whitespace(buf, &mut idx);
+    }
+    toks.push(Token::Eof);
+    toks
+}
+
+fn skip_whitespace(buf: &Vec<char>, idx: &mut usize) {
+    while *idx < buf.len() && buf[*idx].is_whitespace() {
+        *idx += 1;
+    }
 }
 
 fn read_num(buf: &Vec<char>, idx: &mut usize) -> usize {
